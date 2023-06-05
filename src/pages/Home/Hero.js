ccb8ece88b189ca79c37e6fc9edd43b14ad0fa46
unknown <babatundeyusuffolorunso@gmail.com>
2023-06-05 16:32:01 +0100

    adding content

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -9,14 +9,11 @@ const Hero = () => {
               <div className='row align-items-center' style={{padding:'150px 0 50px',overflow:'hidden'}}>
                   <div className='col-12 col-md-6 p-2 p-md-5' style={{overflow:'hidden'}}>
                     <span className='border border-2 rounded-4 px-4 py-1'> <a href='#' className='link '>Cages are now better</a></span>
-                    <h1 className='mt-5'>Eliminate The Risk Of Data Breaches</h1>
+                    <h1 className='mt-5'>Hi,I'm <span id='solak'>SolakGPT</span></h1>
                     {/* <h1 class="animate__animated animate__bounce">An animated element</h1> */}
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,</p>
+                    <p>Discover SolakGPT Ecosystem:<br/>
+                    Where cutting-edge AI meets Defi.out game-changing AI browser and SolakGPT Defi platform redefined the digital web 3.0 landscape 
+                    </p>
                     <button className='btn btn-light text-black'>Get Started</button>
                     <button className='btn  text-white'>Get Intouch</button>
 
